feb38dbdf24505a49557de777ff7cb1fe6824917
OzgunOzdemir <ozgunozdemir92@gmail.com>
2022-01-12 14:59:40 +0300

    lesson-64

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,16 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../data/tours-simple.json`)
   );
 
+exports.checkID = (req, res, next, val) => {
+  if(req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+  next();
+}
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
@@ -16,13 +26,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((item) => item.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
 
   res.status(200).json({
     status: "success",
@@ -67,12 +70,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
 
   res.status(204).json({
     status: "success",
