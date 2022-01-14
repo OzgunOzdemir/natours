8e49d90cae7045db3c02538b958d5718e50c48cc
OzgunOzdemir <ozgunozdemir92@gmail.com>
2022-01-14 11:28:13 +0300

    lesson-82

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
-const app = require('./app');
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL)
+  .connect(DB)
+  .then(() => {
+    console.log("DB connection succesful!");
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
